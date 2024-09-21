f0e2aa923ba7b40e3ecc76ab180a67c21fb4de6f
Huỳnh Thế Hy <hyht9083@ut.edu.vn>
2024-09-21 15:10:15 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,3 @@
 print("chao mọi người mk là thế hY sinh viên data science tấy vui đc gặp mợi người")
 print("mik nhất định sẽ giỏi code và giỏi nhất nhất nhất")
+print("edit trực tiếp trên file này nè")
