d99fd6736f2f53b9175a3a0ebed7614bb97ecb1b
Thehy-IT <hyht9083@ut.edu.vn>
2024-09-30 10:01:07 +0700

    bang 9 chuong

--- bảng_cửu_chương.py
+++ bảng_cửu_chương.py
@@ -1,4 +1,6 @@
-for i in range(2, 10):
-    for j in range(2, 10):
-        print(f"{i} x {j} = {i*j}", end="\t")
-    print()
+def bang_cu_chuong():
+    for i in range(2,10):
+        print(f"bang cuu chuong{i}:\t ")
+        for j in range(2,10):
+            print(f"{i}x{j}={i*j}")
+bang_cu_chuong()
